38d18c2100e370fc4ae8ab459f4b590a897181e7
Brahim Berkasse <brkss@Brahims-MacBook-Pro.local>
2025-02-19 15:44:59 +0100

    gen HTOP using secret and counter , key struct definition !

---
+++ internal/app/htop.go
@@ -0,0 +1,33 @@
+package app
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/binary"
+)
+
+
+func GenerateHOTP(secret string, counter uint64) (uint32, error) {
+
+	var b [8]byte;
+	binary.BigEndian.PutUint64(b[:], counter);
+
+
+	h := hmac.New(sha1.New, []byte(secret));
+	_, err := h.Write(b[:]);
+	if err != nil {
+		return 0, err;
+	}
+	
+	hash := h.Sum(nil);
+
+	offset := hash[len(hash) - 1] & 0x0F;
+	binCode := (uint32(hash[offset]) & 0x7F) << 24 |
+	(uint32(hash[offset+1]) & 0xFF) << 16 |
+	(uint32(hash[offset+2]) & 0xFF) << 8 |
+	(uint32(hash[offset+3]) & 0xFF)
+
+	otp := binCode % 1000000;
+
+	return otp, nil;
+}
---
+++ internal/domain/key.go
@@ -0,0 +1,8 @@
+package domain
+
+
+
+type Key struct {
+	Secret string;
+	Counter uint64;
+}
